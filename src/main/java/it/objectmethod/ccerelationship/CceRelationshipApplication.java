bbc924e3da5c86df734297de4a0492e8529e3637
Gabriele <gabri.28.2015@gmail.com>
2020-05-13 11:04:26 +0200

    start api

---
+++ src/main/java/it/objectmethod/ccerelationship/CceRelationshipApplication.java
@@ -0,0 +1,13 @@
+package it.objectmethod.ccerelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CceRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CceRelationshipApplication.class, args);
+	}
+
+}
